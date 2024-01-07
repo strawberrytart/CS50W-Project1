1dc5fc46e6b10c075fda513b0a2a48a0c26dd573
strawberrytart <jasonleeweihan@gmail.com>
2024-01-07 10:35:46 +0800

    Prepare files for Vercel deployment

--- wiki/wsgi.py
+++ wiki/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wiki.settings')
 
 application = get_wsgi_application()
+
+#bridge connection to Vercel app
+app = application
